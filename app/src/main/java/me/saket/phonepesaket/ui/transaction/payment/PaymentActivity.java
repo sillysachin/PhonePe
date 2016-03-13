242a94804584ef003296259cfdc0423b9746d4be
Saket <saket@saket.me>
2016-03-13 16:32:09 +0530

    Add empty PaymentActivity under /ui/transaction/payment package

---
+++ app/src/main/java/me/saket/phonepesaket/ui/transaction/payment/PaymentActivity.java
@@ -0,0 +1,16 @@
+package me.saket.phonepesaket.ui.transaction.payment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.saket.phonepesaket.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+
+}
