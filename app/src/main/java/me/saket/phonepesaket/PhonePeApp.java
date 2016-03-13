2b8b02cd02b58eedc4b66b8a182500e96d25696a
Saket <saket@saket.me>
2016-03-14 02:12:43 +0530

    Move PhonePeApp outside of \data package
    Had accidentally placed it there.

--- app/src/main/java/me/saket/phonepesaket/PhonePeApp.java
+++ app/src/main/java/me/saket/phonepesaket/PhonePeApp.java
@@ -1,9 +1,10 @@
-package me.saket.phonepesaket.data;
+package me.saket.phonepesaket;
 
 import android.app.Application;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import me.saket.phonepesaket.data.LocalDataRepository;
 
 /**
  * This is the first class that gets created when the app starts
@@ -19,8 +20,8 @@ public class PhonePeApp extends Application {
 
     private void setupLocalDataRepository() {
         Realm.setDefaultConfiguration(new RealmConfiguration.Builder(this)
-                .name(DataRepository.DATABASE_NAME)
-                .schemaVersion(DataRepository.DATABASE_VERSION)
+                .name(LocalDataRepository.DATABASE_NAME)
+                .schemaVersion(LocalDataRepository.DATABASE_VERSION)
                 .setModules(Realm.getDefaultModule())
                 .build());
     }
