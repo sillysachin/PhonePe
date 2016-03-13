a40e6edd2b9fe1b5debdb5b9d86e70d5bcafa1dd
Saket <saket@saket.me>
2016-03-14 01:24:19 +0530

    Add empty PhonePeApp (sub-class of Application class)

---
+++ app/src/main/java/me/saket/phonepesaket/data/PhonePeApp.java
@@ -0,0 +1,16 @@
+package me.saket.phonepesaket.data;
+
+import android.app.Application;
+
+/**
+ * This is the first class that gets created when the app starts
+ * and lives through-out the life of the app.
+ */
+public class PhonePeApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
